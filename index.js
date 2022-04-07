2551e44900666975f40509a0dd4d366bba4b0fd9
Isabella <isabella.y.hu@gmail.com>
2022-04-07 23:56:55 +1000

    commit tree 3cea5720fefda77260cc0f892cd5548a40ea766e

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue<=400) {
+    return 'This one is on me!';
+  } else if (someValue>2500) {
+    return 'No can do.';
+  } else if (someValue>2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(someValue){
   // Write your code here!
+  return (someValue==='NYC' ? 'Ok, sounds good.' : 'No go.');
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(someValue){
   // Write your code here!
+  switch(someValue){
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+      break;
+  }
 }
